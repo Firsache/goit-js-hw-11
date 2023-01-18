002d78c7b2face3666a8b298f5dcc20d3d7f4990
Firsova Liubov <firsova.lyub@gmail.com>
2023-01-18 14:05:01 +0200

    add pagination and onLoadMoreBtnClick

--- src/index.js
+++ src/index.js
@@ -9,6 +9,12 @@ import { renderGallery } from './createGalleryMarkUp';
 
 const pixabayApi = new PixabayAPI();
 
+let simpLightbox = new SimpleLightbox('.photo-card a', {
+    captions: true,
+    captionsData: 'alt',
+    captionDelay: 250,
+});
+
 const refs = {
     formEl: document.querySelector('#search-form'),
     galleryEl: document.querySelector('.gallery'),
@@ -19,39 +25,66 @@ refs.formEl.addEventListener('submit', onSubmitSearchImages)
 
 async function onSubmitSearchImages(evt) {
     evt.preventDefault();
-
+    // console.log(evt.target.elements.searchQuery.value.trim());
     pixabayApi.query = evt.target.elements.searchQuery.value.trim();
     pixabayApi.page = 1;
 
     try {
         const { data } = await pixabayApi.fetchImages();
-        if (data.totalHits.length === 0) {
-        Notify.failure("Sorry, there are no images matching your search query. Please try again.");
-        evt.target.reset();
-        refs.galleryEl.innerHTML = '';
+        console.log(data);
+        console.log(data.totalHits);// число
+        console.log(data.hits);// массив объектов
+
+        if (data.totalHits === 0) {
+            Notify.failure("Sorry, there are no images matching your search query. Please try again.");
+            evt.target.reset();
+            refs.galleryEl.innerHTML = '';
 
         return;
+        } else {
+            Notify.success(`Horray! We found ${data.totalHits} images.`);
+            refs.galleryEl.innerHTML = '';
+            refs.galleryEl.innerHTML = renderGallery(data.hits);
+            simpLightbox.refresh();
+
+            const { height: cardHeight } = document
+            .querySelector(".gallery")
+            .firstElementChild.getBoundingClientRect();
+
+            window.scrollBy({
+            top: cardHeight * 2,
+            behavior: "smooth",
+            });
         }
 
-        data.hits()
-        .then(response => {
-        return renderGallery(response)
-        })
-
-        if (data.total_pages > 1) {
-            loadMoreBtnEl.classList.remove('is-hidden');
-        }
-    } catch(err => console.log(err)
+        if (data.hits.length > 40) {
+            refs.loadMoreBrn.classList.remove('is-hidden');
+        } else {
+            refs.loadMoreBrn.classList.add('is-hidden');
+        }       
+        
+    } catch(error) {
+        console.log(error);
+    }
 }
 
-refs.loadMoreBrn('click', onLoadMoreClick)
+// refs.loadMoreBrn('click', onLoadMoreClick)
 
-async function onLoadMoreClick() {
-    pixabayApi.page += 1;
+// async function onLoadMoreClick(evt) {
+//     pixabayApi.page += 1;
 
-}
+//     const { data } = await pixabayApi.fetchImages();
+//     refs.galleryEl.innerHTML = '';
+//      refs.galleryEl.innerHTML = renderGallery(data.hits);
+//     simpLightbox.refresh();
 
-// 
-// Notify.failure("We're sorry, but you've reached the end of search results.")
-// Notify.success("Horray! We found ${} images.")
+//     // if ( page===total_page ) {
+//     //     
+//     //     Notify.failure("We're sorry, but you've reached the end of search results.")
+//     //      evt.target.classList.remove('is-hidden');
+//     // ============================================================
+//     //       evt.target.hidden = true;
+//     //     
+//     // }
+// }
 
--- src/pixabay-api.js
+++ src/pixabay-api.js
@@ -3,16 +3,49 @@
 import axios from 'axios';
 
 export class PixabayAPI {
+    // const BASE_URL = 'https://pixabay.com/api/';
+    // const API_KEY = '32927001-bdf8cd2697946056c57f9b5f5';
+
+    // constructor() {
+    //     this.page = 1;
+    //     this.query = null;
+    // }
+
+    // async fetchImages(query) {
+    //     // if (query) {
+    //     //     this.query = query;
+    //     // }
+
+    //     const searchParams = {
+    //         params: {
+    //             query: this.query,
+    //             image_type: 'photo',
+    //             orientation: 'horizontal',
+    //             safesearch: true,
+    //             page: this.page,
+    //             per_page: 40,
+    //             key: API_KEY
+    //         }
+            
+    //     }
+    //     const response = await axios.get(`${BASE_URL}`, searchParams)
+    //     return response;
+    // }
+
     static BASE_URL = 'https://pixabay.com/api/';
     static API_KEY = '32927001-bdf8cd2697946056c57f9b5f5';
 
-    constructor() {
+    constructor() {        
         this.page = 1;
         this.query = null;
     }
 
-    fetchImages() {
-        const searchParams = new URLSearchParams{
+    async fetchImages(query) {
+        if (query) {
+            this.query = query;
+        }
+
+        const searchParams = {
             params: {
                 query: this.query,
                 image_type: 'photo',
@@ -20,10 +53,12 @@ export class PixabayAPI {
                 safesearch: true,
                 page: this.page,
                 per_page: 40,
-                client_id: PixabayAPI.API_KEY
+                key: PixabayAPI.API_KEY
             }
-            return axios.get(`${PixabayAPI.BASE_URL}`, searchParams)
-        }
+            
+        }        
+        const response = await axios.get(`${PixabayAPI.BASE_URL}`, searchParams)
+        return response;
     }
 }
 
